a38720b23176e3b8c620fdca7a37ea84dc1d254c
Sanket Khapake <157945367+san7499@users.noreply.github.com>
2025-03-02 01:49:58 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://food-del-pro.onrender.com'
+export const currency = '₹'
 export const assets ={
     logo,
     add_icon,
