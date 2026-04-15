7d293e00f89a9c1033f2fc4868fc6acb3dc522a5
Sanket Khapake <157945367+san7499@users.noreply.github.com>
2026-04-15 21:44:31 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,18 +14,27 @@ const App = () => {
 
   return (
     <>
-      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : null}
+
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+
       <div className="app">
+
         <Navbar setShowLogin={setShowLogin} />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+
           <Route path="/order" element={<PlaceOrder />} />
+
           <Route path="/verify" element={<Verify />} />
           <Route path="/myorders" element={<MyOrders />} />
 
+          <Route path="*" element={<Home />} />
         </Routes>
       </div>
+
+
       <Footer />
     </>
   );
