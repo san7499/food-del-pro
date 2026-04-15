7d130d74eca99fa811846901f54865e0c37e5c74
Sanket Khapake <157945367+san7499@users.noreply.github.com>
2026-04-15 22:29:55 +0530

    Refactor order schema to use ObjectId and structured items

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,15 +1,48 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    userId:{type:String,required:true},
-    items:{type:Array,required:true},
-    amount:{type:Number,required:true},
-    address:{type:Object,required:true},
-    status:{type:String,default:"Food Processing"},
-    date:{type:Date,default:Date.now()},
-    pyament:{type:Boolean,default:false}
-})
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
 
-const orderModel = mongoose.models.order || mongoose.model("order",orderSchema);
+  items: [
+    {
+      _id: String,
+      name: String,
+      price: Number,
+      quantity: Number,
+    },
+  ],
+
+  amount: {
+    type: Number,
+    required: true,
+  },
+
+  address: {
+    type: Object,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    default: "Food Processing",
+  },
+
+  payment: {
+    type: Boolean,
+    default: false,
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const orderModel =
+  mongoose.models.order || mongoose.model("order", orderSchema);
 
 export default orderModel;
